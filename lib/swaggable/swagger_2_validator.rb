e4372c8eeafa45711367e90b8d7f14f7c31b1aa4
Manuel Morales <manuelmorales@gmail.com>
2015-06-30 10:38:24 +0100

    Fixed schema relative path

--- lib/swaggable/swagger_2_validator.rb
+++ lib/swaggable/swagger_2_validator.rb
@@ -2,15 +2,27 @@ require 'json-schema'
 
 module Swaggable
   class Swagger2Validator
-    def self.schema
-      @schema = File.dirname(__FILE__) + '/../../assets/swagger-2.0-schema.json'
+    def self.validate! swagger
+      preload_draft4
+      JSON::Validator.validate!(schema, swagger)
     end
 
-    def self.validate! swagger
-      # This will cache draft4 to avoid downloading it
-      JSON::Validator.validate!(JSON.parse(File.read('assets/json-schema-draft-04.json')), {})
+    private
 
-      JSON::Validator.validate!(schema, swagger)
+    def self.schema
+      @schema = assets_dir + '/swagger-2.0-schema.json'
+    end
+
+    def self.draft4
+      @draft4 = assets_dir + '/json-schema-draft-04.json'
+    end
+
+    def self.assets_dir
+      File.dirname(__FILE__) + '/../../assets'
+    end
+
+    def self.preload_draft4
+      @draft4_preloaded ||= JSON::Validator.validate!(draft4, {})
     end
   end
 end
