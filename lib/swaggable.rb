85215429a39e4335ca0842d2eea89da8d71ac615
Manuel Morales <manuelmorales@gmail.com>
2015-08-12 16:27:41 +0100

    Reorganized autoloads

--- lib/swaggable.rb
+++ lib/swaggable.rb
@@ -1,26 +1,37 @@
 require 'swaggable/version'
 
 module Swaggable
+  # Rack apps
+  autoload :RackApp, 'swaggable/rack_app'
+  autoload :RackRedirect, 'swaggable/rack_redirect'
+  autoload :ValidatingRackApp, 'swaggable/validating_rack_app'
+
+  # Definitions
+  autoload :DefinitionBase, 'swaggable/definition_base'
   autoload :ApiDefinition, 'swaggable/api_definition'
   autoload :EndpointDefinition, 'swaggable/endpoint_definition'
-  autoload :ParameterDefinition, 'swaggable/parameter_definition'
   autoload :TagDefinition, 'swaggable/tag_definition'
+  autoload :ParameterDefinition, 'swaggable/parameter_definition'
   autoload :ResponseDefinition, 'swaggable/response_definition'
   autoload :MimeTypeDefinition, 'swaggable/mime_type_definition'
   autoload :MimeTypesCollection, 'swaggable/mime_types_collection'
-  autoload :RackApp, 'swaggable/rack_app'
-  autoload :GrapeAdapter, 'swaggable/grape_adapter'
-  autoload :GrapeEntityTranslator, 'swaggable/grape_entity_translator'
-  autoload :Swagger2Serializer, 'swaggable/swagger_2_serializer'
-  autoload :Swagger2Validator, 'swaggable/swagger_2_validator'
-  autoload :EndpointIndex, 'swaggable/endpoint_index'
-  autoload :DefinitionBase, 'swaggable/definition_base'
-  autoload :EnumerableAttributes, 'swaggable/enumerable_attributes'
   autoload :SchemaDefinition, 'swaggable/schema_definition'
   autoload :AttributeDefinition, 'swaggable/attribute_definition'
-  autoload :RackRedirect, 'swaggable/rack_redirect'
-  autoload :Errors, 'swaggable/errors'
-  autoload :ValidatingRackApp, 'swaggable/validating_rack_app'
+
+  # Grape
+  autoload :GrapeAdapter, 'swaggable/grape_adapter'
+  autoload :GrapeEntityTranslator, 'swaggable/grape_entity_translator'
+
+  # Swagger
+  autoload :Swagger2Serializer, 'swaggable/swagger_2_serializer'
+  autoload :Swagger2Validator, 'swaggable/swagger_2_validator'
+
+  # Validators
   autoload :ApiRackValidator, 'swaggable/api_rack_validator'
   autoload :EndpointRackValidator, 'swaggable/endpoint_rack_validator'
+  autoload :RequestContentTypeRackValidator, 'swaggable/request_content_type_rack_validator'
+
+  # Others
+  autoload :Errors, 'swaggable/errors'
+  autoload :EnumerableAttributes, 'swaggable/enumerable_attributes'
 end
