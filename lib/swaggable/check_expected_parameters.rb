a0c74c7a2707bb0bfa77137c34c5ab04d438f669
Manuel Morales <manuelmorales@gmail.com>
2015-08-27 15:42:28 +0100

    Simpler check expected parameters

--- lib/swaggable/check_expected_parameters.rb
+++ lib/swaggable/check_expected_parameters.rb
@@ -15,24 +15,9 @@ module Swaggable
 
     def errors
       Errors::ValidationsCollection.new.tap do |errors|
-        errors_for_expected_parameters_in_query.each {|e| errors << e }
-      end
-    end
-
-    private
-
-    def errors_for_expected_parameters_in_query
-      endpoint_p = endpoint.parameters.select {|p| p.location == :query }
-      request_p = request.query_parameters
-
-      errors_for_expected_parameters endpoint_p, request_p
-    end
-
-    def errors_for_expected_parameters endpoint_p, request_p
-      [].tap do |errors|
-        request_p.each do |name, value|
-          unless endpoint_p.detect{|p| p.name == name }
-            errors << Errors::Validation.new("Unexpected parameter #{name.inspect}")
+        request.parameters(endpoint).each do |parameter|
+          unless endpoint.parameters.include? parameter
+            errors << Errors::Validation.new("Unexpected parameter #{parameter.inspect}")
           end
         end
       end
